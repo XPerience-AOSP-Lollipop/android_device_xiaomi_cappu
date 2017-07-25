48dc33def7f35740efcb07ae9db6f923b4bac764
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-07-25 18:13:05 -0500

    cappu:Audio related configs
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- device.mk
+++ device.mk
@@ -22,6 +22,49 @@ PRODUCT_AAPT_CONFIG := normal
 # Dalvik/HWUI
 $(call inherit-product, frameworks/native/build/tablet-7in-xhdpi-2048-dalvik-heap.mk)
 
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+   $(LOCAL_KERNEL):prebuilt/kernel
+
+
+# Bluetooth                            
+PRODUCT_PACKAGES += \
+    audio.a2dp.default 
+PRODUCT_PACKAGES += \
+    libbt-vendor
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio_policy.default \
+    audio_policy.stub \
+    audio.r_submix.default \
+    audio.usb.default \
+    libaudio-resampler \
+    tinymix \
+    libtinyalsa
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/audio/Conf_MultipleTest.ini:system/etc/Conf_MultipleTest.ini \
+    $(LOCAL_PATH)/audio/Conf_MultipleTest_0x01.ini:system/etc/Conf_MultipleTest_0x01.ini \
+    $(LOCAL_PATH)/audio/Conf_MultipleTest_0x02:system/etc/Conf_MultipleTest_0x02 \
+    $(LOCAL_PATH)/audio/default_volume_tables.xml:system/etc/default_volume_tables.xml \
+    $(LOCAL_PATH)/audio/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/audio/media_codecs_mediatek_audio.xml:system/etc/media_codecs_mediatek_audio.xml \
+    $(LOCAL_PATH)/audio/media_codecs_mediatek_video.xml:system/etc/media_codecs_mediatek_videos.xml \
+    $(LOCAL_PATH)/audio/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/audio/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/audio/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml
+
+
 PRODUCT_PACKAGES += \
    libmtk_symbols \
    libstlport
